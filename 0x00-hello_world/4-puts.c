699674cfd7819a7b7f411dc8c2dbaf2a594bd57d
AmberbirMehabaw <-l>
2022-09-11 08:47:20 +0000

    changes

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
-int main(main)
+/**
+ * *  main - prints some message
+ * *
+ * *  Uses: Puts function to print the message
+ * *  Return: 0
+ * */
+
+
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
+			
