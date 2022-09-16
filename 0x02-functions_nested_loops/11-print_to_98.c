70829e90391d7e191f240a7bfb790d9e58bf6b4f
AmberbirMehabaw <mehabawamberbir@gmail.com>
2022-09-16 07:04:34 -0700

    fix print 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,13 +14,13 @@ void print_to_98(int n)
 	{
 		for (i = n ; i < 98; i++)
 			printf("%d, ", i);
-			printf("98\n");
+		printf("98\n");
 	}
 	else if (n > 98)
 	{
 		for (i = n ; i > 98; i--)
 			printf("%d, ", i);
-			printf("98\n");
+		printf("98\n");
 	}
 	if (n == 98)
 		printf("%d\n", n);
