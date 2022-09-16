03de2d9f131c741d282033541fc1a476878ac3ec
AmberbirMehabaw <mehabawamberbir@gmail.com>
2022-09-16 06:42:38 -0700

    fixed

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,12 +9,15 @@ void jack_bauer(void)
 	int hrs, min;
 
 	for (hrs = 0; hrs < 24; hrs++)
-	{
-		_putchar('0' + (hrs / 10));
-		_putchar('0' + (hrs % 10));
-		_putchar(':');
-		_putchar('0' + (min / 10));
-		_putchar('0' + (min % 10));
-		_putchar('\n');
+	{	
+		for (min = 0; min < 60; min++)
+		{
+			_putchar('0' + (hrs / 10));
+			_putchar('0' + (hrs % 10));
+			_putchar(':');
+			_putchar('0' + (min / 10));
+			_putchar('0' + (min % 10));
+			_putchar('\n');
+		}
 	}
 }
