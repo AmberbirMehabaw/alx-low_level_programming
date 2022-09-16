07f389ede517c6ad71c323439cab4eea78c14832
AmberbirMehabaw <mehabawamberbir@gmail.com>
2022-09-16 04:49:39 -0700

    fix

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,10 +9,10 @@ int main(void)
 	char c;
 	for (c = 'a'; c <= 'z'; z++)
 	{
-		if (c =='q' || c == 'e')
-			continue;
-		putchar(c);
+		if (!((c =='q') || (c == 'e')))
+			putchar(c);
 	}
 	putchar('\n');
+
 	return (0);
 }
