fb4425ed410fe4c1f9744e53f8e3c89a23c5f94c
AmberbirMehabaw <mehabawamberbir@gmail.com>
2022-09-16 07:00:22 -0700

    fix

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * add - adds two integer numbers
- * @first: first argument to be added
- * @second: second argument to be added
- * Return: returns the result
- */
+  * add - adds two integer numbers
+  * @first: first argument to be added
+  * @second: second argument to be added
+  * Return: returns the result
+  */
 
 int add(int first, int second)
 {
