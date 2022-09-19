14e87196a3526a11ee728997e7ab29ffca200fd1
AmberbirMehabaw <mehabawAmberbir@gmail.com>
2022-09-19 12:08:33 +0000

    returning a pointer to swap

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies string from source to destination
+ * @dest: an argument as destination of the string
+ * @src: an argument as source of the string
+ * Return: returns dest with success
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+
+	int lemgth = 0;
+
+	for (; *src != '\0'; length++)
+	{
+		dest[length] = src[length];
+	}
+
+	dest[length] = '\0';
+
+	return (dest);
+}
