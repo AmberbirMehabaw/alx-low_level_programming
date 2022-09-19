df4a636ecd4a212e4b6a1d6c7c4f5d91b4049611
AmberbirMehabaw <mehabawAmberbir@gmail.com>
2022-09-19 11:39:59 +0000

    skipping one

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,10 +9,13 @@ void puts2(char *str)
 {
 	int length;
 
-	for (length = 0; str[length] != '\0';)
+	for (length = 0; str[length] != '\0'; length++)
 	{
+		if (count % 2 == 0)
+		{
+			_putchar(str[count]);
 		_putchar(str[length]);
-		length = length + 2;
+		}
 	}
 	_putchar('\n');
 }
