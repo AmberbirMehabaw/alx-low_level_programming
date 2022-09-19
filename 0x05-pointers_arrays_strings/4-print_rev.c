eeed1d203dfe0dbe0e26657cc69133f1e5a3f313
AmberbirMehabaw <mehabawAmberbir@gmail.com>
2022-09-19 10:12:07 +0000

    reverse printing

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,24 +2,25 @@
 
 /**
  * print_rev - print strings in reverse
- * @s: an argument
+ * @s: an argument 
  */
-
 void print_rev(char *s)
 {
-	int length;
+	int length = 0;
 	int index;
+
 	while (*(s + length) != '\0')
 	{
 		length++;
 	}
 
 	index = length - 1;
-	
+
 	while (index >= 0)
 	{
 		_putchar(*(s + index));
 		index--;
 	}
+
 	_putchar('\n');
 }
