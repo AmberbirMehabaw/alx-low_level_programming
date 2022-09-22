2f651d11fec7ed9af3bd2b194810071399d5c490
AmberbirMehabaw <mehabawamberbir@gmail.com>
2022-09-22 01:22:51 -0700

    rot encryption algorithm

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rot13 - encrypts the string to its rot13 version
+ * @s: a string argument to be encrypted
+ * Return: returns a pointer to encrypted message
+ */
+
+char *rot13(char *s)
+{
+	int i = 0, j;
+
+	for (; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] >= 'a' && s[i] <= 'z' || s[i] >= 'A' && s[i] <= 'Z')
+			{
+				s[i] += 13;
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
