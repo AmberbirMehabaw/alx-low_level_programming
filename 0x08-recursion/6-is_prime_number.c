837568f1cf0500962c123a906989d4b101b85665
AmberbirMehabaw <mehabawamberbir@gmail.com>
2022-09-28 01:53:38 -0700

    is prime or not

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+int test(int n, int check);
+
+/**
+ * is_prime_number- checks if n is prime number
+ * @n: number to be checked
+ * Return: return 1 if n is prime or -1 if not
+ */
+int is_prime_number(int n);
+{
+	int check = 2;
+
+	if (n <= 1)
+		return (0);
+	return (test(n, check));
+}
+/**
+ * test - tests whether the number is divisible or not.
+ * @check: The divisor
+ * Return: returns 0 if the number is divisible,
+ * otherwise it returns 1
+ */
+
+int test(int n, int check)
+{
+	if (n % check == 0)
+		return (0);
+	if (check == n / 2)
+		return (1);
+	return (test(n, check + 1));
+}
