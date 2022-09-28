4ecc5a8481e37977d939156aba4a6b554b79a019
AmberbirMehabaw <mehabawamberbir@gmail.com>
2022-09-28 02:20:33 -0700

    wild-card

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -14,6 +14,7 @@ int wildcmp(char *s1, char *s2);
 int strlen_no_wilds(char *str)
 {
 	int len = 0, index = 0;
+
 	if (*(str + index))
 	{
 		if (*str != '*')
@@ -41,13 +42,16 @@ void iterate_wild(char **wildstr)
  * postfix_match - Checks if a string str matches the postfix of
  * @str: The string to be matched.
  * @postfix: The postfix.
- * Return- returns
+ *  Return: If str and postfix are identical - a pointer to the null byte
+ *  located at the end of postfix. Otherwise - a pointer to the first
+ *  unmatched character in postfix.
  */
 
 char *postfix_match(char *str, char *postfix)
 {
 	int str_len = strlen_no_wilds(str) - 1;
 	int postfix_len = strlen_no_wilds(postfix) - 1;
+
 	if (*postfix == '*')
 		iterate_wild(&postfix);
 	if (*(str + str_len - postfix_len) == *postfix && *postfix != '\0')
